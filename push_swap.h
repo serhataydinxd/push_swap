111c488b639d5b56f083cbbaf6a8765ceac591b4
Serhat AYDIN <seraydin@student.42istanbul.com.tr>
2026-08-13 20:05:12 +0300

    doubly linked list struct added

---
+++ push_swap.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seraydin <seraydin@student.42istanbul.com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/13 20:01:02 by seraydin          #+#    #+#             */
+/*   Updated: 2026/08/13 20:04:46 by seraydin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+typedef struct s_node
+{
+	int				data;
+	struct s_node	*next;
+	struct s_node	*prev;
+}					t_node;
+
+#endif
