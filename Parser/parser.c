6b98d067334405151b8fd5ca8a4db9d82fa5ae1d
Ugur Polat <ugpolat@k1m05s09.42istanbul.com.tr>
2026-08-14 06:25:08 +0300

    parser genel olarak düzeltildi bazı edge caseler duruyor ve şu an total 6 fonksiyon var main duruyor denemek için sonrasında atoi geliştirilmiş halini başka bir .c ile açıp bir .h kütüphanesine dahil edip o .a dan make ile alıp kullanabiliriz parser kendi kütüphanesine sahip olabilir belki

--- Parser/parser.c
+++ Parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: ugpolat@student.42istanbul.com.tr          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/14 02:31:37 by ugpolat           #+#    #+#             */
-/*   Updated: 2026/08/14 03:53:14 by ugpolat          ###   ########.fr       */
+/*   Updated: 2026/08/14 06:21:51 by ugpolat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,60 @@ int	ft_atoi(const char *nptr)
 			sign *= -1;
 		i++;
 	}
-	while (nptr[i] >= '0' && nptr[i] <= '9')
+	if (!(nptr[i] >= '0' && nptr[i] <= '9'))
+		exit(1);
+	while (nptr[i] >= '0' && nptr[i] <= '9' && check_max_min(result, nptr[i],
+			sign))
 	{
 		result = (result * 10) + (nptr[i] - '0');
 		i++;
 	}
+	if (!(nptr[i] >= '0' && nptr[i] <= '9') && nptr[i])
+		exit(1);
 	return (result * sign);
 }
 
-t_node	make_link_list(int argc, char **argv)
+int	check_max_min(int result, char a, int sign)
+{
+	long int	min;
+
+	min = (long)__INT_MAX__ + 1;
+	if ((result > ((__INT_MAX__) - (a - '0')) / 10))
+		return (0);
+	if (result > (min - (a - '0')) / 10 && sign == -1)
+		return (0);
+	return (1);
+}
+
+void	free_failed_malloc(t_node *head)
+{
+	t_node	*temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+	exit(1);
+}
+void	check_any_duplicate(t_node *head)
+{
+	t_node	*temp;
+
+	while (head)
+	{
+		temp = head->next;
+		while (temp)
+		{
+			if (head->data == temp->data)
+				exit(1);
+			temp = temp->next;
+		}
+		head = head->next;
+	}
+}
+t_node	*make_link_list(int argc, char **argv)
 {
 	t_node	*head;
 	t_node	*end;
@@ -47,9 +92,11 @@ t_node	make_link_list(int argc, char **argv)
 	i = 1;
 	end = NULL;
 	head = NULL;
-	while (argc - 1 > 0)
+	while (--argc > 0)
 	{
 		temp = malloc(sizeof(t_node));
+		if (!temp)
+			free_failed_malloc(head);
 		temp->next = NULL;
 		temp->data = ft_atoi(argv[i]);
 		temp->prev = NULL;
@@ -60,7 +107,6 @@ t_node	make_link_list(int argc, char **argv)
 		temp->prev = end;
 		end = temp;
 		i++;
-		argc--;
 	}
 	return (head);
 }
@@ -70,6 +116,7 @@ t_node	*parser(int argc, char **argv)
 	t_node	*head;
 
 	head = make_link_list(argc, argv);
+	check_any_duplicate(head);
 	return (head);
 }
 int	main(int argc, char **argv)
