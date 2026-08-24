604e68319c87bd22c146ff7e3f9afd5e1601566b
Serhat AYDIN <seraydin@student.42istanbul.com.tr>
2026-08-24 12:06:27 +0300

    bench changed

--- print_bench.c
+++ print_bench.c
@@ -6,7 +6,7 @@
 /*   By: ugpolat@student.42istanbul.com.tr          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/21 19:13:21 by seraydin          #+#    #+#             */
-/*   Updated: 2026/08/23 19:24:47 by seraydin         ###   ########.fr       */
+/*   Updated: 2026/08/24 12:02:39 by seraydin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,11 @@ static void	print_strategy(t_config *c)
 	if (!c->st_set || c->strategy == ADAPTIVE)
 	{
 		if (c->a_strategy == SIMPLE)
-			ft_printf(2, "[bench] strategy: Adaptive / O(n^2)\n");
+			ft_printf(2, "[bench] strategy: Adaptive - Simple / O(n^2)\n");
 		else if (c->a_strategy == MEDIUM)
-			ft_printf(2, "[bench] strategy: Adaptive / O(n*sqrt(n))\n");
+			ft_printf(2, "[bench] strategy: Adaptive - Medium / O(n*sqrt(n))\n");
 		else if (c->a_strategy == COMPLEX)
-			ft_printf(2, "[bench] strategy: Adaptive / O(n*log(n))\n");
+			ft_printf(2, "[bench] strategy: Adaptive - Complex / O(n*log(n))\n");
 	}
 }
 
